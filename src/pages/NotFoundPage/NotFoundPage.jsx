412b8a2272d8b87304f25181a70f0aa77d543a34
Yelyzaveta Heorhiieva <heorhiieva.y@gmail.com>
2025-02-19 17:02:19 +0200

    add counter

--- src/pages/NotFoundPage/NotFoundPage.jsx
+++ src/pages/NotFoundPage/NotFoundPage.jsx
@@ -1,10 +1,29 @@
+import { useEffect, useState } from 'react';
 import s from './NotFoundPage.module.css'
 import { PiSmileyMeltingFill } from "react-icons/pi";
+import { useNavigate } from 'react-router-dom';
 
 const NotFoundPage = () => {
+
+  const [counter, setCounter] = useState(10);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const id = setInterval(() => {
+      setCounter(prev => prev - 1)
+    }, 1000)
+
+    if (counter === 0) {
+   navigate('/')
+    }
+    
+    return () => clearInterval(id);
+  }, [counter])
+  
+
   return (
     <p className={s.text}>
-      Page is not found <PiSmileyMeltingFill className={s.icon} />
+      Page is not found, after <span className={s.counter}>{counter}s</span>you will be redirected to the home page<PiSmileyMeltingFill className={s.icon} />
     </p>
   )
 }
